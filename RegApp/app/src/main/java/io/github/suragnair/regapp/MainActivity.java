c363e61eff6ef5492c5846b64c98af89020d2ed3
Karan <k.chauhan2910@gmail.com>
2016-01-13 16:52:09 +0530

    changes MainActivity.java

--- RegApp/app/src/main/java/io/github/suragnair/regapp/MainActivity.java
+++ RegApp/app/src/main/java/io/github/suragnair/regapp/MainActivity.java
@@ -10,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    fadsjbjav
-    asjdkbavb
-            bafjskv
-    vabdsfjvka
-                    bajsvka
-    afdjldfhal
-                            adskjlkfjads
-    
+
 }
