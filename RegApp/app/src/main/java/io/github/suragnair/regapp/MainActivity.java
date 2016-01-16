0d389edf9413cd54cb4189bab31803ec57da2c7e
Karan <k.chauhan2910@gmail.com>
2016-01-17 02:13:04 +0530

    Added Live Background to App---Changes made in Main_Activity.java and Main_Activity.xml--Png files Added

--- RegApp/app/src/main/java/io/github/suragnair/regapp/MainActivity.java
+++ RegApp/app/src/main/java/io/github/suragnair/regapp/MainActivity.java
@@ -1,7 +1,11 @@
 package io.github.suragnair.regapp;
 
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.service.wallpaper.WallpaperService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ivLoader = (ImageView) findViewById(R.id.IVloadinganimation);
+        ivLoader.setBackgroundResource(R.layout.live_bg);
+
+
+        AnimationDrawable frameAnimation = (AnimationDrawable) ivLoader.getBackground();
+        frameAnimation.start();
     }
 
-    
 }
